c03d05772ed90d5549de7eaa73f16db43069b959
jazzyjz <jasminf2202@gmail.com>
2024-02-20 19:52:20 -0500

    added form for input

--- WordCounter/assets/script.js
+++ WordCounter/assets/script.js
@@ -4,8 +4,13 @@ var textSpace = document.querySelector('#textSpace')
 
 addText.addEventListener('click', textInput)
 
+//function to input word 
 function textInput (){ 
    var addTextEL = document.createElement("div");
    textSpace.appendChild(addTextEL);
+
+   var input = prompt('ples enter a word')
+
+   textSpace.textContent = input
    
 }

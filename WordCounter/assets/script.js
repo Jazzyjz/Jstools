dc4934e8411408c4461ada10cd730e45e2fc8ee3
MugenJigo <os.romero1995@gmail.com>
2024-02-26 19:31:31 -0500

    added word counter function

--- WordCounter/assets/script.js
+++ WordCounter/assets/script.js
@@ -8,8 +8,7 @@ addText.addEventListener('click', textInput)
 //function to input word 
 function textInput (e){ 
    e.preventDefault();
-   var addTextEL = document.createElement("div");
-   textSpace.appendChild(addTextEL);
+   
 
    var word = input.value;
    textSpace.textContent = word;
@@ -22,11 +21,14 @@ function textInput (e){
 //function to count letters in word
 
 function wordcounter(word){
+   var wordCounterEL = document.createElement("div")
+   textSpace.appendChild(wordCounterEL);
    // evaluate how many letters in word
    let length = word.length;
-
-   const words = word.split(' ');
-   console.log(words.length);
-
+   const words = word.split(' ') 
+ console.log(words.length)
    console.log(length);
+
+   wordCounterEL.textContent = words.length 
+   
 }
